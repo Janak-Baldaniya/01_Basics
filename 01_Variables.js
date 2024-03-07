0b6f7f394b73cfe52f3f18cb2333b12d2b74d405
Janak-Baldaniya <baldaniyajanak1826@gmail.com>
2024-03-07 17:53:04 +0530

    first commit

---
+++ 01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1412
+let accountemail = "jnk@123"
+var accountpassword = "123456"
+accountcity = "junagadh"
+
+
+// accountId = 2 not allowed
+console.log(accountId);
+
+console.table([accountId, accountemail, accountpassword, accountcity])
+
+let loginid = 2233
+console.log(loginid);
---
+++ 02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"  //treat all js code as new version
+
+//alert(3 + 3) //we are using node js , not browser
+
+let name = "janak"
+let age = 18
+let state; //undefind 
+
+
+//Types 
+// 1) number
+// 2) string
+// 3) bigint
+// 4) null
+// 5) boolean => true/false
+// 6) undefind => 
+// 7) symbol => unique
+
+// 8) object
+
+
+console.log(typeof "janak");  // use to find datatype
+console.log(typeof null);
